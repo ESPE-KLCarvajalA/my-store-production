f5eec21e82b4331de4eb0f97e89ecfe70e155324
ESPE-KLCarvajalA <135269362+ESPE-KLCarvajalA@users.noreply.github.com>
2025-03-06 20:44:21 -0500

    sequelize2

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,23 +1,15 @@
-//libs/sequelize.js
 const { Sequelize } = require('sequelize');
-const { config } = require('./../config/config');
-const initModels = require('./../db/models');
-const USER = encodeURIComponent(config.dbUser);
-const PASS = encodeURIComponent(config.dbPass);
-const URI = `postgres://${USER}:${PASS}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-const sequelize = new Sequelize(URI, {
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: (msg) => console.log(msg),
+  logging: console.log,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     },
   },
-
 });
-
-initModels(sequelize);
-
 
 module.exports = sequelize;
